08d9f69a9969c09ace2c30a7f009f76988cae52b
Filip Jirsák <filip@jirsak.org>
2021-04-26 18:30:31 +0200

    Dokončení komponenty Card.

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import './style.css';
 const Card = (props) => {
   return `
     <div class="card">
-      <div class="card__title">
-        ${props.title}
+      <div class="card__head">
+        ${props.head}
+      </div>
+      <div class="card__text">
+        ${props.text}
       </div>
     </div>
   `;
 };
 
 const appElm = document.querySelector('#app');
-appElm.innerHTML += Card({ title: 'Doručení ještě dnes' });
+appElm.innerHTML += Card({ head: 'Doručení ještě dnes' });
 
